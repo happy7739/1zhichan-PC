6f8ac4c89661d84412cf3fa7f0c116f609ae4052
happy7739 <773916571@qq.com>
2020-07-01 11:52:48 +0800

    修改数据库名称

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'dolphin',
+    'database'        => '1zhichan',
     // 用户名
     'username'        => 'root',
     // 密码
